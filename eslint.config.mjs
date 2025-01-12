723ef3217b09394eef8c99730adbbe9cef550cd1
Marek Kugacz <marek@kugacz.com>
2025-01-12 22:53:40 +0100

    Eslint rules update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,21 +6,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, love, eslintConfigPrettier, {
     files: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
     rules: {
-        '@typescript-eslint/no-explicit-any': ['off'],
         'no-console': ['off'],
-        '@typescript-eslint/array-type': [
-            'error',
-            {
-                default: 'generic',
-            },
-        ],
-        '@typescript-eslint/strict-boolean-expressions': [
-            'error',
-            {
-                allowNullableObject: true,
-                allowAny: true,
-            },
-        ],
     },
     languageOptions: {
         parserOptions: {
